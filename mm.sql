39f3d1e65f1711f06d28c4347d05fe74f5fbb453
ZenSynthysian <111430913+ZenSynthysian@users.noreply.github.com>
2024-02-20 13:42:29 +0700

    add sql

---
+++ mm.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 20 Feb 2024 pada 07.40
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sparepart`
+--
+
+CREATE TABLE `sparepart` (
+  `id` varchar(50) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `harga` int(30) NOT NULL,
+  `foto` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `sparepart`
+--
+
+INSERT INTO `sparepart` (`id`, `nama`, `harga`, `foto`) VALUES
+('1', 'Jalu Stang (Weight A Handle) – PCX 150 K97 & PCX Hybrid', 25500, 'https://www.hondacengkareng.com/wp-content/uploads/2019/02/53102K97T00-GAMBAR.jpg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
